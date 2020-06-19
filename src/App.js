5c5b9d0d407efd1152068bb23517a6e0a172ca4f
SHAN SHAJI <shanshaji@gmail.com>
2020-06-19 12:08:06 +0530

    Added Header and About page.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Header from './components/header';
+import About from './components/about';
+// import Resume from './components/Resume';
+// import Contact from './components/Contact';
+// import Footer from './components/Footer';
+import {main,resume} from './resumeData.json';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      
+      resumeData:{
+        main: main,
+        resume: resume,
+      }
+    }
+  }
+
+
+
+
+  render() {
+    console.log(main);
+    return (
+      <div className="App">
+        <Header data={main} />
+        <About data={this.state.resumeData.main} />
+        {/* <Resume data={this.state.resumeData.resume} />
+        <Contact data={this.state.resumeData.main} />
+        <Footer /> */}
+      </div>
+    );
+  }
 }
 
 export default App;
