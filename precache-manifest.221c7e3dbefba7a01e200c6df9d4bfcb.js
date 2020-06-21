deb0c7480a5fd742bff38aaad8a3c0078d65a74f
SHAN SHAJI <shanshaji@gmail.com>
2020-06-21 18:32:19 +0530

    Updates

---
+++ precache-manifest.221c7e3dbefba7a01e200c6df9d4bfcb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0f7196b2aa99dee897c67e35e2731a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0da6f3694058528286e6",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "d26e3673b710ac6d1fad",
+    "url": "/static/js/2.fa588d20.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fa588d20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0da6f3694058528286e6",
+    "url": "/static/js/main.5719344c.chunk.js"
+  },
+  {
+    "revision": "3ffbed3ab286b0362d22",
+    "url": "/static/js/runtime-main.c39bd313.js"
+  },
+  {
+    "revision": "999eeb6662dd34453721e94cb14b73ab",
+    "url": "/static/media/resume.999eeb66.pdf"
+  }
+]);
